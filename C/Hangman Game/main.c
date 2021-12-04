bbe32ca7e579b80938db503b4f0f4c3cf2d15be3
Achatin <37224828+Achatin@users.noreply.github.com>
2021-12-04 20:20:38 +0100

    Update main.c

--- C/Hangman Game/main.c
+++ C/Hangman Game/main.c
@@ -10,31 +10,8 @@ int main() {
 
     char secret[50];
 
-    printf("Secret... %d\n", get_word(secret));
-
-    printf("%d\n", is_word_guessed("weaseled", "abcdefghijklmnopqrstuvwxyz"));
-    printf("%d\n", is_word_guessed("secret", ""));
-    printf("%d\n", is_word_guessed("main", "amarwertgvbin"));
-
-
-    char result[30];
-    get_guessed_word("container", "", result);
-    printf("%s\n", result);
-
-    char dostupne[30];
-//    char pouzite_pismena[30];
-//    int cyklenie = 0;
-//    while(1) {
-//        get_available_letters(pouzite_pismena, dostupne);
-//        scanf("%s\n", &pouzite_pismena[cyklenie]);
-//        printf("Available letters: %s\n", dostupne);
-//        cyklenie++;
-//    }
-
-
-    get_available_letters("abcdefghijklmnopqrstu", dostupne);
-    printf("%s\n", dostupne);
-
+    get_secret(secret);
+   
     hangman(secret);
 
     return 0;
